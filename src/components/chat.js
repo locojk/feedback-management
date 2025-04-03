ecc1264b8c3530eece5786d334c10d866090301f
Mingyang-ZHAI <zmyswufe@163.com>
2025-04-02 21:51:59 -0400

    Fix API endpoint and trigger rebuild

--- src/components/chat.js
+++ src/components/chat.js
@@ -61,7 +61,7 @@ export default function Chat() {
 
     try {
       const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-      const response = await fetch(`${BASE_URL}/chatbot/textchatbot/`, {
+      const response = await fetch(`${BASE_URL}/chatbot/`, {
       // const response = await fetch("http://localhost:8000/chatbot/textchatbot/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
