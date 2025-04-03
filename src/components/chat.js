1f02f728a1166b64b8fbabf24d2708051ebfaebb
Mingyang-ZHAI <zmyswufe@163.com>
2025-04-02 22:19:19 -0400

    Fix: use /chatbot/ and rebuild with env

--- src/components/chat.js
+++ src/components/chat.js
@@ -61,6 +61,7 @@ export default function Chat() {
 
     try {
       const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+      console.log("BASE_URL is:", BASE_URL);
       const response = await fetch(`${BASE_URL}/chatbot/`, {
       // const response = await fetch("http://localhost:8000/chatbot/textchatbot/", {
         method: "POST",
